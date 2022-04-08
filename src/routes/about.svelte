60f96c6c5ca8e7d09daf9b82dd09a16b00bb9d75
Christopher <chris_bernhard@yahoo.com>
2022-04-07 21:12:46 -0500

    added about

---
+++ src/routes/about.svelte
@@ -0,0 +1,12 @@
+<div class="about">
+  <h2>About Us</h2>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus eveniet est, sit debitis neque earum consectetur beatae ullam maxime molestias iste aperiam numquam saepe dolores vitae blanditiis enim? Cumque dolore similique itaque ut, nisi minima porro, sunt eligendi, quidem laborum ea nesciunt vero. Repudiandae cupiditate ipsum laborum earum qui facilis sequi consectetur, excepturi modi mollitia vel beatae eligendi consequuntur dolore.</p>
+</div>
+
+<style>
+  .about {
+    text-align: center;
+    display: block;
+    margin: 2rem auto;
+  }
+</style>
