ebf50e634e408af25e8b9d742a29eba2832a291d
Christopher Bernhard <chris_bernhard@yahoo.com>
2022-04-08 18:39:29 +0000

    add global css and layout

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,8 +1,8 @@
 <script>
-  import Title from '$lib/title.svelte'
+
 </script>
 
-<Title title="Ninja Gaming Reviews" />
+
 <div class="about">
   <h2>About Us</h2>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus eveniet est, sit debitis neque earum consectetur beatae ullam maxime molestias iste aperiam numquam saepe dolores vitae blanditiis enim? Cumque dolore similique itaque ut, nisi minima porro, sunt eligendi, quidem laborum ea nesciunt vero. Repudiandae cupiditate ipsum laborum earum qui facilis sequi consectetur, excepturi modi mollitia vel beatae eligendi consequuntur dolore.</p>
