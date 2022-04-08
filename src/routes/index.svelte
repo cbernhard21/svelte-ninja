0850f766346f4ed5697efe0fb80d445af4ca0480
Christopher Bernhard <chris_bernhard@yahoo.com>
2022-04-08 18:21:10 +0000

    worked on routing

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,16 @@
 <script>
-
+  import Title from '$lib/title.svelte';
 </script>
+
+
+<Title />
 
 <div class="index">
   <h2>Welcome</h2>
   <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias, nulla. Non quod, ipsa beatae aliquam, reprehenderit nihil accusamus enim quasi obcaecati sequi cum! Exercitationem, repudiandae qui at modi consequatur velit, fuga odit voluptatibus iusto, suscipit consequuntur voluptatem vero excepturi. Temporibus ullam, possimus corporis et sunt reiciendis neque aliquid officia maiores culpa totam, enim doloribus quibusdam laborum ad, qui dolorem praesentium.</p>
+
+  <a href="/guides">View Guides</a>
+  <a href="/about">About</a>
 </div>
 
 <style>
