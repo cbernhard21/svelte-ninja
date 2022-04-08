625cbf6f0ba9f722b061e867173abff22eadf87a
Christopher <chris_bernhard@yahoo.com>
2022-04-07 21:09:29 -0500

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<div class="index">
+  <h2>Welcome</h2>
+  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias, nulla. Non quod, ipsa beatae aliquam, reprehenderit nihil accusamus enim quasi obcaecati sequi cum! Exercitationem, repudiandae qui at modi consequatur velit, fuga odit voluptatibus iusto, suscipit consequuntur voluptatem vero excepturi. Temporibus ullam, possimus corporis et sunt reiciendis neque aliquid officia maiores culpa totam, enim doloribus quibusdam laborum ad, qui dolorem praesentium.</p>
+</div>
+
+<style>
+  .index {
+    text-align: center;
+    display: block;
+    margin: 2rem auto;
+  }
+</style>
